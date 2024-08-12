3aa19c0c5efb5591d1330f04c6466f5c31e72a5c
Harmeet Singh <er.harmeetsingh@hotmail.com>
2024-08-11 21:27:30 -0600

    Renamed test files

--- tests/todo-app.spec.ts
+++ tests/todo-app.spec.ts
@@ -1,39 +1,83 @@
-import { test, expect, type Page } from '@playwright/test';
-import { checkNumberOfCompletedTodosInLocalStorage, checkNumberOfTodosInLocalStorage, checkTodosInLocalStorage } from '../src/todo-app';
+import test from "@lib/BaseTest";
 
-test.beforeEach(async ({ page }) => {
-  await page.goto('https://demo.playwright.dev/todomvc');
+test.beforeEach(async ({ page, todoApp }) => {
+  await page.goto("https://todomvc.com/examples/react/dist");
 });
 
-const TODO_ITEMS = [
-  'complete code challenge',
-  'ensure coverage for all items is automated'
-];
+// Test # 1 - Add a new ToDo item
+test.describe("Test#1- Create ToDo item", () => {
+  test("Create a ToDo item and verify item added at the end", async ({ todoApp }) => {
+    await todoApp.addANewToDo("Task1");
+    await todoApp.addANewToDo("Task2");
+    await todoApp.addANewToDo("Task3");
+    await todoApp.verifyTaskListCountMatch(3);
+    await todoApp.verifyTaskWithTextExist("Task2");
+    await todoApp.verifyFooterTaskStatusMatches("3 items left");
+    await todoApp.verifyLastItemHasText("Task3");
+  });
+});
 
-test.describe('Create New Todo', () => {
-  test('should be able to create new items on the page', async ({ page }) => {
-    // create a new todo locator
-    const newTodo = page.getByPlaceholder('What needs to be done?');
+// Test # 2 - Edit an existing todo item
+test.describe("Test#2- Edit ToDo item", () => {
+  test("Edit an existing task", async ({ todoApp }) => {
+    await todoApp.addANewToDo("Task1");
+    await todoApp.addANewToDo("Task2");
+    await todoApp.addANewToDo("Task3");
+    await todoApp.verifyFooterTaskStatusMatches("3 items left");
+    await todoApp.editTask("Task2","New Task 2")
+    await todoApp.verifyTaskWithTextExist("New Task 2");
+  });
+});
 
-    // Create 1st todo.
-    await newTodo.fill(TODO_ITEMS[0]);
-    await newTodo.press('Enter');
+// Test # 3 - Delete a todo item using cross
+test.describe("Test#3 - Delete tasks", () => {
+  test("Delete a task from the list", async ({ todoApp }) => {
+    await todoApp.addANewToDo("Task1");
+    await todoApp.addANewToDo("Task2");
+    await todoApp.addANewToDo("Task3");
+    await todoApp.verifyTaskWithTextExist("Task1");
+    await todoApp.verifyFooterTaskStatusMatches("3 items left");
+    await todoApp.deleteTask('Task1');
+    await todoApp.verifyFooterTaskStatusMatches("2 items left");
+  });
+});
 
-    // Make sure the list only has one todo item.
-    await expect(page.getByTestId('todo-title')).toHaveText([
-      TODO_ITEMS[0]
-    ]);
+// Test # 4 - Mark a task as completed
+test.describe("Test#4- Mark task as completed", () => {
+  test("Mark a task as completed", async ({ todoApp }) => {
+    await todoApp.addANewToDo("Task1");
+    await todoApp.addANewToDo("Task2");
+    await todoApp.addANewToDo("Task3");
+    await todoApp.verifyFooterTaskStatusMatches("3 items left");
+    await todoApp.completeTask("Task1")
+    await todoApp.verifyFooterTaskStatusMatches("2 items left");
+    await todoApp.verifyTaskIsMarkedCompleted("Task1");
+  });
+});
 
-    // Create 2nd todo.
-    await newTodo.fill(TODO_ITEMS[1]);
-    await newTodo.press('Enter');
+// Test # 5 - Given the active list, only the active tasks are shown
+test.describe("Test#5- Filter active list", () => {
+  test("Complete a task and apply filter to get only active tasks", async ({ todoApp }) => {
+    await todoApp.addANewToDo("Task1");
+    await todoApp.addANewToDo("Task2");
+    await todoApp.addANewToDo("Task3");
+    await todoApp.completeTask("Task1")
+    await todoApp.verifyTaskListCountMatch(3);
+    await todoApp.filterActiveTasks();
+    await todoApp.verifyTaskListCountMatch(2);
+    await todoApp.verifyTaskDoesNotExist("Task1");
+  });
+});
 
-    // Make sure the list now has two todo items.
-    await expect(page.getByTestId('todo-title')).toHaveText([
-      TODO_ITEMS[0],
-      TODO_ITEMS[1]
-    ]);
-
-    await checkNumberOfTodosInLocalStorage(page, 2);
+// Test # 6 - When clear completed is clicked, item removed (note that completed isnt moved to the completed list after clearing)
+test.describe("Test#6- Clear completed tasks", () => {
+  test("Complete a task and clear completed tasks", async ({ todoApp }) => {
+    await todoApp.addANewToDo("Task1");
+    await todoApp.addANewToDo("Task2");
+    await todoApp.addANewToDo("Task3");
+    await todoApp.completeTask("Task1")
+    await todoApp.verifyTaskListCountMatch(3);
+    await todoApp.clearCompletedTasks();
+    await todoApp.verifyTaskListCountMatch(2);
   });
 });
